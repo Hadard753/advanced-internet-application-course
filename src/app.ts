cfdfd2bbb611ae05eb08442eb4bf98b8ab43bd76
Hadar Dayan <hdayan@tenable.com>
2024-01-06 23:42:03 +0200

    add docker and mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import express, { Request, Response } from 'express';
 import * as dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 import userRoutes from './routes';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Connect to MongoDB
+console.log(`connecting mongo db`);
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World! My name is Dayan');

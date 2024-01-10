db59e0581db0f7b17cd043dc565e22b4a10e4dba
Hadar Dayan <hdayan@tenable.com>
2024-01-10 23:09:25 +0200

    add login functionality

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 import User, { UserDocument } from "../models/user.model";
 
@@ -14,13 +15,37 @@ const sendError = (res: Response, code: number, message: string) => {
 
 
 export const login = async (req: Request, res: Response, next: NextFunction) => {
-    console.log("login");
-    sendError(res, 400, 'not implemented')
+    try {
+        console.log(req.body);
+        const { username, password } = req.body;
+
+        if (!username || !password) {
+            return sendError(res, 400, 'Invalid credentials');
+        }
+
+        const user: UserDocument | null = await User.findOne({ username });
+
+        if (!user) {
+            return res.status(401).json({ message: 'User not found' });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordMatch) {
+            return sendError(res, 401, 'Invalid password')
+        }
+
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET || "", { expiresIn: '1h' });
+
+        return res.status(200).json({ message: 'Login successful', token });
+    } catch (error) {
+        // Handle any errors that occur during the login process
+        console.error('Error during login:', error);
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
 }
 
 export const register = async (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.body);
-
     const { username, password } = req.body;
 
     if (!username || !password) sendError(res, 402, 'invalid username or password');
@@ -33,14 +58,13 @@ export const register = async (req: Request, res: Response, next: NextFunction) 
     const hashedPassword = await bcrypt.hash(password, saltRounds);
 
     const newUser: UserDocument = new User({
-      username,
-      password: hashedPassword,
+        username,
+        password: hashedPassword,
     });
 
     await newUser.save();
 
     return res.status(201).json({ message: 'User registered successfully' });
-
 }
 
 export const logout = async (req: Request, res: Response, next: NextFunction) => {
